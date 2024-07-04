e0e3d1c5f1678d520577468c477de047cdaaaa60
Harsh <harshchamola@gmail.com>
2024-07-04 16:54:37 +0400

    update

--- src/GRID/Gridcomponent.js
+++ src/GRID/Gridcomponent.js
@@ -1,11 +1,15 @@
-import React,  {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Grid } from '@mui/material';
 import { styled } from '@mui/system';
- 
+
 class Gridcomponent extends Component {
     render() {
         const { columnCount, spacing, width, height, children } = this.props;
+
+        //this is representing columncount which is an array of rows and columns
+        const [rows, columns] = columnCount;
+        const totalCells = rows * columns;
 
         const GridContainer = styled(Grid)({
             width: width,
@@ -15,26 +19,25 @@ class Gridcomponent extends Component {
         });
 
         const GridItem = styled(Grid)(({ theme }) => ({
-            flexBasis: `calc(100% / ${columnCount} - ${spacing * 2}px)`,
+            flexBasis: `calc(100% / ${columns} - ${spacing * 2}px)`,
             margin: spacing,
             boxSizing: 'border-box',
         }));
 
         return (
             <GridContainer container>
-                {React.Children.map(children, (child) => (
-                    <GridItem item>
+                {React.Children.map(children, (child, index) => (
+                    <GridItem item key={index}>
                         {child}
                     </GridItem>
-                ))}
+                )).slice(0, totalCells)}
             </GridContainer>
         );
     }
 }
 
-
 Gridcomponent.propTypes = {
-    columnCount: PropTypes.number,
+    columnCount: PropTypes.arrayOf(PropTypes.number).isRequired,
     spacing: PropTypes.number,
     width: PropTypes.string,
     height: PropTypes.string,
@@ -42,7 +45,7 @@ Gridcomponent.propTypes = {
 };
 
 Gridcomponent.defaultProps = {
-    columnCount: 3,
+    columnCount: [3, 3],
     spacing: 8,
     width: '100%',
     height: '100%',
