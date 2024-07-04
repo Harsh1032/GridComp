bf3bc5a7390800638f749c33dbd91a6029d19436
Harsh <harshchamola@gmail.com>
2024-07-04 16:54:24 +0400

    update data

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,21 +1,23 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react';
 import { createRoot } from 'react-dom/client';
 import Gridcomponent from '../../src/GRID/Gridcomponent'
-import Example from '../../src'
 
-export default class Demo extends Component {
+class Demo extends Component {
   render() {
     return (
       <div>
-       <Gridcomponent columnCount={4} spacing={8} width="100%" height="480px">
-        <div style={{background: 'blue'}}>Item 1</div>
-        <div style={{background: 'purple'}}>Item 2</div>
-        <div style={{background: 'red'}}>Item 3</div>
-        <div style={{background: 'green'}}>Item 4</div>
-      </Gridcomponent>
-    </div>
-    )
-    
+       <Gridcomponent columnCount={[4, 2]} spacing={8} width="100%" height="480px">
+          <div style={{background: 'blue'}}>Item 1</div>
+          <div style={{background: 'purple'}}>Item 2</div>
+          <div style={{background: 'red'}}>Item 3</div>
+          <div style={{background: 'green'}}>Item 4</div>
+          <div style={{background: 'yellow'}}>Item 5</div>
+          <div style={{background: 'pink'}}>Item 6</div>
+          <div style={{background: 'orange'}}>Item 7</div>
+          <div style={{background: 'brown'}}>Item 8</div>
+        </Gridcomponent>
+      </div>
+    );
   }
 }
 
